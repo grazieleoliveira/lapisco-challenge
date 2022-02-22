fb1a0d839950e8c3d94a914e02a200ce3f09b8b0
Graziele Oliveira <hypixelgrazi@gmail.com>
2022-02-22 11:41:15 -0300

    full code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import SearchIcon from "@material-ui/icons/Search";
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get(`https://randomuser.me/api/?results=26`);
+      setData(res.data?.results);
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (!search) {
+      setFilteredData(data);
+    }
+  }, [search, data]);
+
+  const handleFilter = (event) => {
+    const searchWord = event.target.value;
+    setSearch(searchWord);
+    const newFilter = data?.filter((value) => {
+      const fullName = value.name.first + "" + value.name.last;
+      return fullName.toLowerCase().includes(searchWord.toLowerCase());
+    });
+    if (searchWord) {
+      setFilteredData(newFilter);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="page-title">Desafio Front-end - LAPISCO</p>
+      <div className="results-container">
+        <div className="input-container">
+          <div className="searchIcon">
+            <SearchIcon />
+          </div>
+          <input value={search} type="text" onChange={handleFilter} />
+        </div>
+        {filteredData?.length !== 0 && (
+          <div className="results">
+            {filteredData?.slice(0, 15).map((item, key) => {
+              return (
+                <div key={key} className="item">
+                  <img id="user-image" src={item.picture.large} alt="user" />
+                  <div className="desc-container">
+                    <p id="card-title">
+                      {item.name.first} {item.name.last}{" "}
+                    </p>
+                    <p className="card-subtitle">
+                      {item.gender === "female" ? "Feminino" : "Masculino"} -{" "}
+                      {item.dob.age} anos
+                    </p>
+                    <p className="card-subtitle">{item.cell}</p>
+                    <p id="card-email">{item.email}</p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
